aa5d9c2584910a766d245d78fd8031c814b71e64
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-08-09 11:07:34 +0600

    practices

---
+++ React JS/React Main Concepts/C-7 Hooks use state/Hooks Use State/src/App.js
@@ -0,0 +1,14 @@
+import Fhook from './components/Fhook.js'
+import React from 'react'
+
+// import Hooks from './components/hooks.js'
+
+
+export default function App() {
+  return (
+    <div> 
+      
+      <Fhook/>
+    </div>
+  )
+}
---
+++ React JS/React Main Concepts/C-7 Hooks use state/React Form/src/App.js
@@ -0,0 +1,10 @@
+import Form from './components/form/form.js'
+import React from 'react'
+
+export default function App() {
+  return (
+    <div> 
+      <Form/>
+    </div>
+  )
+}
